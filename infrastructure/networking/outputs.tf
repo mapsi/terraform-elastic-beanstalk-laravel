07b9a99b74efa0882cdbd8be036cda7cbb776481
Angel Psiakis <angel@mml.rocks>
2020-03-24 18:50:07 +0000

    componetise network

--- infrastructure/networking/outputs.tf
+++ infrastructure/networking/outputs.tf
@@ -10,5 +10,22 @@ output "private_subnet_cidrs" {
 
 output "vpc_cidr" {
   value       = module.vpc.vpc_cidr_block
-  description = "VPC ID"
+  description = "VPC CIDR"
+}
+
+output "vpc_default_security_group_id" {
+  value       = module.vpc.vpc_default_security_group_id
+  description = "VPC SG ID"
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.subnets.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.subnets.private_subnet_ids
 }
