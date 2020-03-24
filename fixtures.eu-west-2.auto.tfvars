da5d0ab938c9c10a914bb436b5324d047098e9b3
Angel Psiakis <angel@mml.rocks>
2020-03-24 19:52:31 +0000

    add redis

--- fixtures.eu-west-2.auto.tfvars
+++ fixtures.eu-west-2.auto.tfvars
@@ -126,3 +126,20 @@ db_parameter_group = "mysql5.7"
 publicly_accessible = false
 
 apply_immediately = true
+
+
+## REDIS ##
+
+instance_type_redis = "cache.t2.micro"
+
+cluster_size = 1
+
+family = "redis4.0"
+
+engine_version_redis = "4.0.10"
+
+at_rest_encryption_enabled = false
+
+transit_encryption_enabled = true
+
+zone_id = "Z3SO0TKDDQ0RGG"
--- main.tf
+++ main.tf
@@ -90,6 +90,7 @@ module "elastic_beanstalk_environment" {
     "DB_SCHEMA"   = var.database_name
     "DB_USERNAME" = var.database_user
     "DB_PASSWORD" = var.database_password
+    "REDIS_HOST"  = module.redis.cluster_host
   }
 }
 
@@ -126,6 +127,36 @@ module "rds_instance" {
       name         = "sort_buffer_size"
       value        = "2097152"
       apply_method = "immediate"
+    }
+  ]
+}
+
+
+### REDIS ###
+
+module "redis" {
+  source                     = "git::https://github.com/cloudposse/terraform-aws-elasticache-redis.git?ref=tags/0.16.0"
+  availability_zones         = var.availability_zones
+  namespace                  = var.namespace
+  stage                      = var.stage
+  name                       = var.name
+  zone_id                    = var.zone_id
+  vpc_id                     = module.networking.vpc_id
+  allowed_security_groups    = [module.networking.vpc_default_security_group_id]
+  subnets                    = module.networking.private_subnet_ids
+  cluster_size               = var.cluster_size
+  instance_type              = var.instance_type_redis
+  apply_immediately          = true
+  automatic_failover_enabled = false
+  engine_version             = var.engine_version_redis
+  family                     = var.family
+  at_rest_encryption_enabled = var.at_rest_encryption_enabled
+  transit_encryption_enabled = var.transit_encryption_enabled
+
+  parameter = [
+    {
+      name  = "notify-keyspace-events"
+      value = "lK"
     }
   ]
 }
